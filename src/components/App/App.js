116f38662186cd3216f051333a110d1f5e7c83db
pgarr <garlej.p@gmail.com>
2021-09-02 20:59:29 +0200

    remove unused icons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faClock,
-  faPlus,
-  faStar as fasFaStar,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
-import { faStar as farFaStar } from "@fortawesome/free-regular-svg-icons";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 import Layout from "../Layout/Layout";
 import RoutesList from "./RoutesList";
@@ -15,10 +9,10 @@ import { FirebaseContext } from "../../firebase/firebase";
 
 const App = () => {
   // awesome icons library
-  library.add(faClock, faPlus, farFaStar, fasFaStar, faUser);
+  library.add(faClock);
 
+  // Firebase setup
   const { api } = useContext(FirebaseContext);
-
   useEffect(() => {
     api.setRecipeListener();
   });
